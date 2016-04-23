28d8e216e6ba28d4083a7e362f586e3825ec1997
Daan van den Berg <info@doppy.nl>
2016-04-23 19:00:38 +0200

    refactor compilers to use baseclass instead of trait

--- DependencyInjection/CompilerPass/FileLocatorCompilerPass.php
+++ DependencyInjection/CompilerPass/FileLocatorCompilerPass.php
@@ -2,16 +2,35 @@
 
 namespace Doppy\PdfGeneratorBundle\DependencyInjection\CompilerPass;
 
-use Doppy\UtilBundle\Helper\CompilerPass\TaggedServicesTrait;
+use Doppy\UtilBundle\Helper\CompilerPass\BaseTagServiceCompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
 
-class FileLocatorCompilerPass implements CompilerPassInterface
+class FileLocatorCompilerPass extends BaseTagServiceCompilerPass
 {
-    use TaggedServicesTrait;
-
-    public function process(ContainerBuilder $container)
+    protected function handleTag(
+        ContainerBuilder $containerBuilder,
+        Definition $serviceDefinition,
+        Reference $taggedServiceReference,
+        $attributes
+    )
     {
-        $this->processTaggedServices($container, 'doppy_pdf_generator.file_locator', 'addLocator');
+        $serviceDefinition->addMethodCall(
+            'addLocator',
+            array(
+                $taggedServiceReference
+            )
+        );
+    }
+
+    protected function getService(ContainerBuilder $containerBuilder)
+    {
+        return $containerBuilder->getDefinition('doppy_pdf_generator.file_locator');
+    }
+
+    protected function getTaggedServices(ContainerBuilder $containerBuilder)
+    {
+        return $containerBuilder->findTaggedServiceIds('doppy_pdf_generator.file_locator');
     }
 }
--- DependencyInjection/CompilerPass/PreProcessorCompilerPass.php
+++ DependencyInjection/CompilerPass/PreProcessorCompilerPass.php
@@ -2,16 +2,35 @@
 
 namespace Doppy\PdfGeneratorBundle\DependencyInjection\CompilerPass;
 
-use Doppy\UtilBundle\Helper\CompilerPass\TaggedServicesTrait;
+use Doppy\UtilBundle\Helper\CompilerPass\BaseTagServiceCompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
 
-class PreProcessorCompilerPass implements CompilerPassInterface
+class PreProcessorCompilerPass extends BaseTagServiceCompilerPass
 {
-    use TaggedServicesTrait;
-
-    public function process(ContainerBuilder $container)
+    protected function handleTag(
+        ContainerBuilder $containerBuilder,
+        Definition $serviceDefinition,
+        Reference $taggedServiceReference,
+        $attributes
+    )
     {
-        $this->processTaggedServices($container, 'doppy_pdf_generator.pre_processor', 'addProcessor');
+        $serviceDefinition->addMethodCall(
+            'addProcessor',
+            array(
+                $taggedServiceReference
+            )
+        );
+    }
+
+    protected function getService(ContainerBuilder $containerBuilder)
+    {
+        return $containerBuilder->getDefinition('doppy_pdf_generator.pre_processor');
+    }
+
+    protected function getTaggedServices(ContainerBuilder $containerBuilder)
+    {
+        return $containerBuilder->findTaggedServiceIds('doppy_pdf_generator.pre_processor');
     }
 }
